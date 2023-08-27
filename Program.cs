596b035d36481cb5429afbfe367fa7b8c9b7959a
Шикалов_Никита <muchik.33@yandex.ru>
2023-08-27 14:41:10 +0300

    done homework

--- Program.cs
+++ Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25. Напишите цикл, который на вход принимает два числа (A) и (B) и возводит число А в степень В
+
+// System.Console.WriteLine(" Введите число А ");
+// int A = Convert.ToInt32(System.Console.ReadLine());
+
+// System.Console.WriteLine(" Введите число В ");
+// int B = Convert.ToInt32(System.Console.ReadLine()); 
+// int result = 1;
+// void getSqrt()
+// {
+//     for (int i = 1; i <= B; i++)
+//     {
+//         result = result*A;
+//         System.Console.Write(result+ " ");
+//     }
+// }
+// getSqrt();
+
+
+// Задача 27. Напишите программу, которая принимает на вход число и выдает сумму цифр в нем
+
+// System.Console.WriteLine(" Введите число ");
+// int num = Convert.ToInt32(System.Console.ReadLine());
+// int result = 0;
+// int A = 0;
+// void sum()
+// {
+//     while (num >= 1 )
+//     {
+//         A = num%10;
+//         System.Console.Write(A+ " ");
+//         num = num/10;
+//         System.Console.Write(num+ " ");
+//         result+=A;
+//     }
+//     System.Console.WriteLine(result);
+// }
+// sum();
+
+
+
+// Задача 29. Напишите программу, которая задает массив из 8 элементов и выводит их на экран 
+
+// void getArray()
+// {
+//     int [] Array = new int [8];
+//     for (int i = 0; i < Array.Length; i++)
+//      {
+//          Array[i] = new Random().Next(0,300);
+//          System.Console.Write(Array[i] + " ");
+// }
+// }
+// getArray();
